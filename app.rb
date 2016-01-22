c74f7cbb8a9b6f7f116d51fad7bd02a7bd7cd64d
Jeff Seymour <seym1585@gmail.com>
2016-01-22 13:43:01 -0800

    add functionality to delete and update client and stylist information

--- app.rb
+++ app.rb
@@ -37,6 +37,31 @@ get('/stylists/:id') do
   erb(:stylist)
 end
 
+delete("/stylists/:id") do
+  @stylist =  Stylist.find(params.fetch("id").to_i())
+  @stylist.delete()
+  @stylists = Stylist.all()
+  erb(:stylists)
+end
+
+get('/stylists/:id/edit_stylist') do
+  @stylist =  Stylist.find(params.fetch("id").to_i())
+  erb(:edit_stylist)
+end
+
+patch("/stylists/:id") do
+  first_name = params.fetch('first_name')
+  last_name = params.fetch('last_name')
+  phone = params.fetch('phone')
+  @stylists = Stylist.all()
+  @stylist = Stylist.find(params.fetch("id").to_i())
+  @stylist.update({first_name: first_name,
+                         last_name: last_name,
+                         phone: phone,
+                         id: nil})
+  erb(:success)
+end
+
 #------------Clients-------------------------
 
 
@@ -60,5 +85,37 @@ post('/clients') do
                        id: nil,
                        stylist_id: stylist_id})
   client.save()
+  erb(:success)
+end
+
+get('/clients/:id') do
+  @client = Client.find(params.fetch('id').to_i())
+  erb(:client)
+end
+
+delete("/clients/:id") do
+  @client =  Client.find(params.fetch("id").to_i())
+  @client.delete()
+  @clients = Client.all()
+  erb(:clients)
+end
+
+get('/clients/:id/edit_client') do
+  @client =  Client.find(params.fetch("id").to_i())
+  erb(:edit_client)
+end
+
+patch("/clients/:id") do
+  first_name = params.fetch('first_name')
+  last_name = params.fetch('last_name')
+  phone = params.fetch('phone')
+  stylist_id = params.fetch('stylist_id').to_i()
+  @clients = Client.all()
+  @client = Client.find(params.fetch("id").to_i())
+  @client.update({first_name: first_name,
+                       last_name: last_name,
+                       phone: phone,
+                       id: nil,
+                       stylist_id: stylist_id})
   erb(:success)
 end
